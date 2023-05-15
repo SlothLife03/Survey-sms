ff8812b09845d444651736a9d729cd3b23ff4520
Eilidh Bennett <85575578+SlothLife03@users.noreply.github.com>
2023-05-15 16:11:23 +0100

    Create variables.tf

---
+++ terraform/modules/data-actions/variables.tf
@@ -0,0 +1,9 @@
+variable "client_id" {
+    type        = string
+    description = "Client id to be used for client credentials"
+}
+
+variable "client_secret" {
+    type        = string
+    description = "Client secret to be used for client credentials"
+}
